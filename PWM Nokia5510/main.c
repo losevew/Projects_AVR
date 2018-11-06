cecc08be4b2da48b3315ce208879a4390f28b9ed
losevew <losevew@mail.ru>
2018-11-06 14:23:57 +0400

    initial commit

---
+++ PWM Nokia5510/main.c
@@ -0,0 +1,253 @@
+/*******************************************************
+This program was created by the
+CodeWizardAVR V3.12 Advanced
+Automatic Program Generator
+© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : 
+Version : 
+Date    : 19.12.2016
+Author  : 
+Company : 
+Comments: 
+
+
+Chip type               : ATmega8A
+Program type            : Application
+AVR Core Clock frequency: 16,000000 MHz
+Memory model            : Small
+External RAM size       : 0
+Data Stack size         : 256
+*******************************************************/
+
+#include <mega8.h>
+
+// Graphic Display functions
+#include <glcd.h>
+#include <stdlib.h>
+
+// Font used for displaying text
+// on the graphic display
+#include "Font5x7ru.h"
+
+// Declare your global variables here
+unsigned char NewState, OldState, OldVol, upState, downState;
+unsigned char Vol = 0x7F;
+
+void EncoderScan(void);
+void LcdOut (unsigned char);
+
+
+// Timer 0 overflow interrupt service routine
+interrupt [TIM0_OVF] void timer0_ovf_isr(void)
+{
+// Reinitialize Timer 0 value
+TCNT0=0x59;
+// Place your code here
+EncoderScan();
+
+}
+
+
+void EncoderScan(void)
+{
+    NewState = PINB & 0x03;
+    if (NewState != OldState) { 
+        switch (OldState) {
+            case 2:
+                {
+                if (NewState == 3)upState++;
+                if (NewState == 0)downState++;
+                break;
+                }
+            case 0:
+                {
+                if (NewState == 2)upState++;
+                if (NewState == 1)downState++;
+                break;
+                }
+            case 1:
+                {
+                if (NewState == 0)upState++;
+                if (NewState == 3)downState++;
+                break;
+                }
+            case 3:
+                {
+                if (NewState == 1)upState++;
+                if (NewState == 2)downState++;
+                break;
+                }
+        }; 
+        OldState = NewState;
+        if (upState >= 4) { 
+            Vol++;
+            upState = 0;
+        };
+        if (downState >= 4) { 
+            Vol--;
+            downState = 0;
+        };
+    
+    };
+
+}
+
+void LcdOut (unsigned char pwVol )
+{
+     char Duty[]; 
+     int bar;
+     bar=pwVol*83/255;
+     glcd_outtextxyf(12,5,"Скважность"); 
+     itoa(pwVol*100/255,Duty);
+     glcd_outtextxy(35,15,Duty);
+     glcd_outtextf("%");
+     glcd_state.fill.color=0;
+     glcd_bar(0,30,83,40);
+     glcd_state.fill.color=1;
+     glcd_bar(0,30,bar,40);                         
+     
+}
+
+void main(void)
+{
+// Declare your local variables here
+// Variable used to store graphic display
+// controller initialization data
+GLCDINIT_t glcd_init_data;
+
+// Input/Output Ports initialization
+// Port B initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=Out Bit3=Out Bit2=In Bit1=In Bit0=In 
+DDRB=(0<<DDB7) | (0<<DDB6) | (0<<DDB5) | (1<<DDB4) | (1<<DDB3) | (0<<DDB2) | (0<<DDB1) | (0<<DDB0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=0 Bit3=0 Bit2=T Bit1=P Bit0=P 
+PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (1<<PORTB1) | (1<<PORTB0);
+
+// Port C initialization
+// Function: Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRC=(0<<DDC6) | (0<<DDC5) | (0<<DDC4) | (0<<DDC3) | (0<<DDC2) | (0<<DDC1) | (0<<DDC0);
+// State: Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTC=(0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
+
+// Port D initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRD=(0<<DDD7) | (0<<DDD6) | (0<<DDD5) | (0<<DDD4) | (0<<DDD3) | (0<<DDD2) | (0<<DDD1) | (0<<DDD0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTD=(0<<PORTD7) | (0<<PORTD6) | (0<<PORTD5) | (0<<PORTD4) | (0<<PORTD3) | (0<<PORTD2) | (0<<PORTD1) | (0<<PORTD0);
+
+// Timer/Counter 0 initialization
+// Clock source: System Clock
+// Clock value: 250,000 kHz
+TCCR0=(0<<CS02) | (1<<CS01) | (1<<CS00);
+TCNT0=0x59;
+
+// Timer/Counter 1 initialization
+// Clock source: System Clock
+// Clock value: Timer1 Stopped
+// Mode: Normal top=0xFFFF
+// OC1A output: Disconnected
+// OC1B output: Disconnected
+// Noise Canceler: Off
+// Input Capture on Falling Edge
+// Timer1 Overflow Interrupt: Off
+// Input Capture Interrupt: Off
+// Compare A Match Interrupt: Off
+// Compare B Match Interrupt: Off
+TCCR1A=(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<WGM11) | (0<<WGM10);
+TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (0<<WGM12) | (0<<CS12) | (0<<CS11) | (0<<CS10);
+TCNT1H=0x00;
+TCNT1L=0x00;
+ICR1H=0x00;
+ICR1L=0x00;
+OCR1AH=0x00;
+OCR1AL=0x00;
+OCR1BH=0x00;
+OCR1BL=0x00;
+
+// Timer/Counter 2 initialization
+// Clock source: System Clock
+// Clock value: 125,000 kHz
+// Mode: Fast PWM top=0xFF
+// OC2 output: Non-Inverted PWM
+// Timer Period: 2,048 ms
+// Output Pulse(s):
+// OC2 Period: 2,048 ms Width: 1,02 ms
+ASSR=0<<AS2;
+TCCR2=(1<<PWM2) | (1<<COM21) | (0<<COM20) | (1<<CTC2) | (1<<CS22) | (0<<CS21) | (1<<CS20);
+TCNT2=0x83;
+OCR2=0x7F;
+
+// Timer(s)/Counter(s) Interrupt(s) initialization
+TIMSK=(0<<OCIE2) | (0<<TOIE2) | (0<<TICIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (0<<TOIE1) | (1<<TOIE0);
+
+// External Interrupt(s) initialization
+// INT0: Off
+// INT1: Off
+MCUCR=(0<<ISC11) | (0<<ISC10) | (0<<ISC01) | (0<<ISC00);
+
+// USART initialization
+// USART disabled
+UCSRB=(0<<RXCIE) | (0<<TXCIE) | (0<<UDRIE) | (0<<RXEN) | (0<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
+
+// Analog Comparator initialization
+// Analog Comparator: Off
+// The Analog Comparator's positive input is
+// connected to the AIN0 pin
+// The Analog Comparator's negative input is
+// connected to the AIN1 pin
+ACSR=(1<<ACD) | (0<<ACBG) | (0<<ACO) | (0<<ACI) | (0<<ACIE) | (0<<ACIC) | (0<<ACIS1) | (0<<ACIS0);
+SFIOR=(0<<ACME);
+
+// ADC initialization
+// ADC disabled
+ADCSRA=(0<<ADEN) | (0<<ADSC) | (0<<ADFR) | (0<<ADIF) | (0<<ADIE) | (0<<ADPS2) | (0<<ADPS1) | (0<<ADPS0);
+
+// SPI initialization
+// SPI disabled
+SPCR=(0<<SPIE) | (0<<SPE) | (0<<DORD) | (0<<MSTR) | (0<<CPOL) | (0<<CPHA) | (0<<SPR1) | (0<<SPR0);
+
+// TWI initialization
+// TWI disabled
+TWCR=(0<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWEN) | (0<<TWIE);
+
+// Graphic Display Controller initialization
+// The PCD8544 connections are specified in the
+// Project|Configure|C Compiler|Libraries|Graphic Display menu:
+// SDIN - PORTC Bit 0
+// SCLK - PORTC Bit 1
+// D /C - PORTC Bit 2
+// /SCE - PORTC Bit 3
+// /RES - PORTC Bit 4
+
+// Specify the current font for displaying text
+glcd_init_data.font=font5x7ru;
+// No function is used for reading
+// image data from external memory
+glcd_init_data.readxmem=NULL;
+// No function is used for writing
+// image data to external memory
+glcd_init_data.writexmem=NULL;
+// Set the LCD temperature coefficient
+glcd_init_data.temp_coef=PCD8544_DEFAULT_TEMP_COEF;
+// Set the LCD bias
+glcd_init_data.bias=PCD8544_DEFAULT_BIAS;
+// Set the LCD contrast control voltage VLCD
+glcd_init_data.vlcd=PCD8544_DEFAULT_VLCD;
+
+glcd_init(&glcd_init_data);
+
+// Global enable interrupts
+#asm("sei")
+
+while (1)
+      {
+      // Place your code here
+      if (Vol != OldVol) {   
+          LcdOut(Vol);
+          OldVol = Vol;
+          OCR2=Vol;
+      };
+
+      }
+}
