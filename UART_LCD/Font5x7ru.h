d00578358b2e50dbcd5b8f5e06ea3758f9ca0568
losevew <losevew@mail.ru>
2018-11-06 15:01:49 +0400

    initial commit

---
+++ UART_LCD/Font5x7ru.h
@@ -0,0 +1,23 @@
+/****************************************************************************
+Font created by the LCD Vision V1.05 font & image editor/converter
+(C) Copyright 2011-2013 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Font name: font5x7ru
+Fixed font width: 5 pixels
+Font height: 7 pixels
+First character: 0x20
+Last character: 0xFF
+
+Exported font data size:
+1572 bytes for displays organized as horizontal rows of bytes
+1124 bytes for displays organized as rows of vertical bytes.
+****************************************************************************/
+
+#ifndef _FONT5X7RU_INCLUDED_
+#define _FONT5X7RU_INCLUDED_
+
+extern flash unsigned char font5x7ru[];
+
+#endif
+
